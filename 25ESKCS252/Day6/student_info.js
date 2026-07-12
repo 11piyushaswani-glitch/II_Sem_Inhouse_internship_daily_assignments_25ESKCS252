9c940f7eab83dfe77285822bc50a32f74164b2dd
11piyushaswani-glitch <11.piyushaswani@gmail.com>
2026-07-12 18:08:22 +0530

    Add files via upload

--- 25ESKCS252/Day6/student_info.js
+++ 25ESKCS252/Day6/student_info.js
@@ -1,1 +1,20 @@
+function searchStudent(){
 
+    let input = document.getElementById("search").value.toLowerCase();
+
+    let cards = document.getElementsByClassName("student-card");
+
+    for(let i=0;i<cards.length;i++){
+
+        let name = cards[i].getAttribute("data-name").toLowerCase();
+
+        if(name.includes(input)){
+            cards[i].style.display="block";
+        }
+        else{
+            cards[i].style.display="none";
+        }
+
+    }
+
+}
