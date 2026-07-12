d1ac3c66c690f9ce5ea5efb248ebbc11f4ada894
11piyushaswani-glitch <11.piyushaswani@gmail.com>
2026-07-12 18:01:46 +0530

    Add files via upload

--- 25ESKCS252/Day4/script.js
+++ 25ESKCS252/Day4/script.js
@@ -1,1 +1,32 @@
+let count = 0;
 
+const counter = document.getElementById("count");
+
+function increase(){
+    count++;
+    counter.innerHTML = count;
+}
+
+function decrease(){
+    count--;
+    counter.innerHTML = count;
+}
+
+function reset(){
+    count = 0;
+    counter.innerHTML = count;
+}
+
+function toggleMode(){
+
+    document.body.classList.toggle("dark");
+
+    const btn = document.querySelector(".toggle");
+
+    if(document.body.classList.contains("dark")){
+        btn.innerHTML = "☀️ Light Mode";
+    }
+    else{
+        btn.innerHTML = "🌙 Dark Mode";
+    }
+}
