289d4f04d42d76ae9ff824cc7ddbd154faa54f13
11piyushaswani-glitch <11.piyushaswani@gmail.com>
2026-07-12 23:00:09 +0530

    Delete 25ESKCS252 directory

--- 25ESKCS252/Day11/sql_update.sql
+++
@@ -1,6 +0,0 @@
-USE students_management;
-
-ALTER TABLE students ADD COLUMN photo VARCHAR(255) DEFAULT NULL;
-
--- Create uploads directory in your project folder
--- mkdir uploads/ (in day11 folder)
